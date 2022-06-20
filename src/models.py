35b92a0183ccabf3e3cc98e8e4b7c77d994cacca
mpadillazeravla <mikepadilla82@gmail.com>
2022-06-20 10:25:07 +0000

    database model

--- src/models.py
+++ src/models.py
@@ -8,23 +8,51 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class user(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
+    username = Column(String(250), unique=True, nullable=False)
     name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), unique=True, nullable=False)
+    password = Column(String, unique=False, nullable=False)
+    post = relationship('post', backref='user', lazy=True)
+    comment = relationship('comment', backref='user', lazy=True)
+    
 
-class Address(Base):
-    __tablename__ = 'address'
+class post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post = relationship('post', backref='comment', lazy=True)
+    post = relationship('post', backref='media', lazy=True)
+    user_id = Column(Integer, ForeignKey('user.id'),
+        nullable=False)
+
+
+class comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'),
+        nullable=False)     
+    post_id = Column(Integer, ForeignKey('post.id'),
+        nullable=False)           
+   
+
+class media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    url = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'),
+        nullable=False)      
+
+     
+
+
 
     def to_dict(self):
         return {}
